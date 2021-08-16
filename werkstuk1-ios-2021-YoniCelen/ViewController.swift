bb8d1e2d04f9ceee4d99e07ff12414ee15b9eabf
Yoni Celen <yoni.celen@student.ehb.be>
2021-08-16 14:43:48 +0200

    Initial Commit

---
+++ werkstuk1-ios-2021-YoniCelen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  werkstuk1-ios-2021-YoniCelen
+//
+//  Created by user201042 on 8/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
